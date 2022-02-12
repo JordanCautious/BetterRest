d3bed6ca120ff08b1337e6f50de6f03acd4d8e09
Jordan Haynes <Jordancautious@gmail.com>
2022-02-12 09:08:19 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jordan Haynes on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
